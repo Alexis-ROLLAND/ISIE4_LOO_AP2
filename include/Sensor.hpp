b3a7a0eba67c1f700602a51ef0ca3d9d44ff7199
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2024-09-21 08:12:26 +0200

    Adding comments

--- include/Sensor.hpp
+++ include/Sensor.hpp
@@ -11,38 +11,67 @@
 template <typename OutputType>
 concept Numeric = std::is_arithmetic_v<OutputType>; /**< Use of concepts to ensure Output value is a numeric type */
 
+/**
+ * Class Sensor
+ *  Abstract class - Template 
+ */
 template <Numeric OutputType>
 class Sensor{
 
 private:
-    const uint32_t UID{};   /**< Each Sensor has a Unique ID (32 bits) */
-    const std::string   Unit{}; /**< Unit of the output  */
-    OutputType outputValue{};
+    const uint32_t UID{};           /**< Each Sensor has a Unique ID (32 bits) */
+    const std::string   Unit{};     /**< Unit of the output, or any message helping to interpret Output Value  */
+    OutputType outputValue{};       /**< OutputValue type is linked to the template type  */
 protected:
     
+    /**
+     * Setter for OutputValue.
+     */
     void setOutputValue(OutputType newValue) noexcept {this->outputValue = newValue;};
     
 public:
     Sensor() = delete;  /**< Ctor by default is deleted */
+    /**
+     * @brief   "Standard" Ctor
+     * 
+     * @param   uint32_t uid : uid for the new Sensor
+     * @param   const std::string &unit : unit for the new sensor     * 
+     *     
+     */
     Sensor(uint32_t uid, const std::string &unit):UID{uid},Unit{unit}{};
+
     virtual ~Sensor() = default;    /**< Dtor is defaulted */
 
+    /**
+     * Getters
+     */
     [[nodiscard]] const std::string getUnit() const noexcept {return this->Unit;};
     [[nodiscard]] uint32_t  getUID() const noexcept {return this->UID;};
     [[nodiscard]] OutputType getOutputValue() const noexcept {return this->outputValue;};
 
+    /**
+     * @brief Call to this function manages the real (physical) sensor to get a new output value. 
+     */
     virtual void    elaborateNewValue() = 0;   /**< Pure virtual method -> class is now abract */ 
 
+    /**
+     *  @brief  Prints the outout value and the unit message.
+     */
     void    dump() const noexcept {std::println("{} {}",this->getOutputValue,this->getUnit());};
 };
 //----------------------------------------------------------------------------
+/**
+ * ADT7310 concrete Sensor class
+ */
 class ADT7310 : public Sensor<uint16_t>{
     public:
-        enum class ADT7310Mode {MODE13BITS, MODE16BITS};
+        enum class ADT7310Mode {    MODE13BITS,     /**< Sensor is used in 13 bits mode */
+                                    MODE16BITS      /**< Sensor is used in 16 bits mode */
+        };
     private:
-        ADT7310Mode  Mode{ADT7310Mode::MODE13BITS};
+        ADT7310Mode  Mode{ADT7310Mode::MODE13BITS}; /**< Sensor mode (13/13 bits) */
     public:
-        ADT7310() = delete;
+        ADT7310() = delete;     /**< Default Ctor is deleted */
         explicit ADT7310(uint32_t uid):Sensor<uint16_t>(uid,"LSB 0.0625°C (Signed 13 bits mode) or LSB 0.0078°C (Signed 16 bits mode)"){};  
         virtual ~ADT7310() = default;
 
