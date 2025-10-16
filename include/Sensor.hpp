495df756dc7a260edacaf86718cab1c79e2794d8
Alexis ROLLAND <alexis.rolland@univ-tours.fr>
2025-10-16 15:03:35 +0200

    Starting update to 2025/2026 version

--- include/Sensor.hpp
+++ include/Sensor.hpp
@@ -1,11 +1,11 @@
 #ifndef __SENSOR_HPP__
 #define __SENSOR_HPP__
 
-#include <cstdint>
-#include <string>
 #include <concepts>
+#include <cstdint>
 #include <print>
-#include <random>   // for the "fakes" sensors 
+#include <random>   // for the "fakes" sensors
+#include <string>
 
 //----------------------------------------------------------------------------
 template <typename OutputType>
@@ -13,116 +13,115 @@ concept Numeric = std::is_arithmetic_v<OutputType>; /**< Use of concepts to ensu
 
 /**
  * Class Sensor
- *  Abstract class - Template 
+ *  Abstract class - Template
  */
-template <Numeric OutputType>
-class Sensor{
+template <Numeric OutputType> class Sensor {
 
-private:
-    const uint32_t UID{};           /**< Each Sensor has a Unique ID (32 bits) */
-    const std::string   Unit{};     /**< Unit of the output, or any message helping to interpret Output Value  */
-    OutputType outputValue{};       /**< OutputValue type is linked to the template type  */
-protected:
-    
+  private:
+    const uint32_t UID{};     /**< Each Sensor has a Unique ID (32 bits) */
+    const std::string Unit{}; /**< Unit of the output, or any message helping to interpret Output Value  */
+    OutputType outputValue{}; /**< OutputValue type is linked to the template type  */
+  protected:
     /**
      * Setter for OutputValue.
      */
-    void setOutputValue(OutputType newValue) noexcept {this->outputValue = newValue;};
-    
-public:
-    Sensor() = delete;  /**< Ctor by default is deleted */
+    void setOutputValue(OutputType newValue) noexcept { this->outputValue = newValue; };
+
+  public:
+    Sensor() = delete; /**< Ctor by default is deleted */
+
     /**
      * @brief   "Standard" Ctor
-     * 
+     *
      * @param   uint32_t uid : uid for the new Sensor
-     * @param   const std::string &unit : unit for the new sensor     * 
-     *     
+     * @param   const std::string &unit : unit for the new sensor     *
+     *
      */
-    Sensor(uint32_t uid, const std::string &unit):UID{uid},Unit{unit}{};
+    Sensor(uint32_t uid, const std::string &unit) : UID{uid}, Unit{unit} {};
 
-    virtual ~Sensor() = default;    /**< Dtor is defaulted */
+    virtual ~Sensor() = default; /**< Dtor is defaulted */
 
     /**
      * Getters
      */
-    [[nodiscard]] const std::string getUnit() const noexcept {return this->Unit;};
-    [[nodiscard]] uint32_t  getUID() const noexcept {return this->UID;};
-    [[nodiscard]] OutputType getOutputValue() const noexcept {return this->outputValue;};
+    [[nodiscard]] const std::string getUnit() const noexcept { return this->Unit; };
+    [[nodiscard]] uint32_t getUID() const noexcept { return this->UID; };
+    [[nodiscard]] OutputType getOutputValue() const noexcept { return this->outputValue; };
 
     /**
-     * @brief Call to this function manages the real (physical) sensor to get a new output value. 
+     * @brief Call to this function manages the real (physical) sensor to get a new output value.
      */
-    virtual void    elaborateNewValue() = 0;   /**< Pure virtual method -> class is now abract */ 
+    virtual void elaborateNewValue() = 0; /**< Pure virtual method -> class is now abract */
 
     /**
      *  @brief  Prints the outout value and the unit message.
      */
-    void    dump() const noexcept {std::println("{} {}",this->getOutputValue,this->getUnit());};
+    void dump() const noexcept { std::println("{} {}", this->getOutputValue(), this->getUnit()); };
 };
 //----------------------------------------------------------------------------
 /**
  * ADT7310 concrete Sensor class
  */
-class ADT7310 : public Sensor<uint16_t>{
-    public:
-        enum class ADT7310Mode {    MODE13BITS,     /**< Sensor is used in 13 bits mode */
-                                    MODE16BITS      /**< Sensor is used in 16 bits mode */
-        };
-    private:
-        ADT7310Mode  Mode{ADT7310Mode::MODE13BITS}; /**< Sensor mode (13/13 bits) */
-    public:
-        ADT7310() = delete;     /**< Default Ctor is deleted */
-        explicit ADT7310(uint32_t uid):Sensor<uint16_t>(uid,"LSB 0.0625°C (Signed 13 bits mode) or LSB 0.0078°C (Signed 16 bits mode)"){};  
-        virtual ~ADT7310() = default;
+class ADT7310 : public Sensor<uint16_t> {
+  public:
+    enum class ADT7310Mode {
+        MODE13BITS, /**< Sensor is used in 13 bits mode */
+        MODE16BITS  /**< Sensor is used in 16 bits mode */
+    };
 
-        void        setMode(ADT7310Mode Mode) noexcept {this->Mode = Mode;};
-        ADT7310Mode getMode() const noexcept {return this->Mode;}; 
+  private:
+    ADT7310Mode _Mode{ADT7310Mode::MODE13BITS}; /**< Sensor mode (13/16 bits) */
+  public:
+    ADT7310() = delete; /**< Default Ctor is deleted */
+    explicit ADT7310(uint32_t uid)
+        : Sensor<uint16_t>(uid, "LSB 0.0625°C (Signed 13 bits mode) or LSB 0.0078°C (Signed 16 bits mode)") {};
+    virtual ~ADT7310() = default;
 
-        virtual void    elaborateNewValue() override;
+    void setMode(ADT7310Mode Mode) noexcept { this->_Mode = Mode; };
+    ADT7310Mode getMode() const noexcept { return this->_Mode; };
 
-        double  getConvertedTemperature() const noexcept;
+    virtual void elaborateNewValue() override;
+
+    double getConvertedTemperature() const noexcept;
 };
 //----------------------------------------------------------------------------
-template <Numeric OutputType>
-class GenericTempCelciusSensor : public Sensor<OutputType>{
-    private:
+template <Numeric OutputType> class GenericTempCelciusSensor : public Sensor<OutputType> {
+  private:
+  public:
+    GenericTempCelciusSensor() = delete;
+    virtual ~GenericTempCelciusSensor() = default;
 
-    public:
-        GenericTempCelciusSensor() = delete;
-        virtual ~GenericTempCelciusSensor() = default;
+    explicit GenericTempCelciusSensor(uint32_t uid) : Sensor<OutputType>(uid, "°C") {};
 
-        explicit GenericTempCelciusSensor(uint32_t uid):Sensor<OutputType>(uid,"°C"){};
-
-        virtual void    elaborateNewValue() = 0;
+    virtual void elaborateNewValue() = 0;
 };
 //----------------------------------------------------------------------------
-constexpr   uint32_t    TC74_UUID{0x7400484F};
+constexpr uint32_t TC74_UUID{0x7400484F};
 
-class TC74 : public GenericTempCelciusSensor<int8_t>{
-    private:
-        const uint8_t i2c_address{};
-    public:
-        TC74() = delete;
-        virtual ~TC74() = default;
-        explicit TC74(uint8_t addr):GenericTempCelciusSensor<int8_t>{TC74_UUID},i2c_address{addr}{};
+class TC74 : public GenericTempCelciusSensor<int8_t> {
+  private:
+    const uint8_t i2c_address{};
 
-        uint8_t getI2Caddress() const noexcept {return this->i2c_address;};
+  public:
+    TC74() = delete;
+    virtual ~TC74() = default;
+    explicit TC74(uint8_t addr) : GenericTempCelciusSensor<int8_t>{TC74_UUID}, i2c_address{addr} {};
 
-        virtual void    elaborateNewValue() override;
+    uint8_t getI2Caddress() const noexcept { return this->i2c_address; };
+
+    virtual void elaborateNewValue() override;
 };
 //----------------------------------------------------------------------------
-constexpr   uint32_t    LM35_UUID{0x35000000};
+constexpr uint32_t LM35_UUID{0x35000000};
 
-class LM35 : public GenericTempCelciusSensor<double>{
-    private:
-        
-    public:
-        LM35():GenericTempCelciusSensor<double>{LM35_UUID}{};
-        virtual ~LM35() = default;
-        
-        virtual void    elaborateNewValue() override;
+class LM35 : public GenericTempCelciusSensor<double> {
+  private:
+  public:
+    LM35() : GenericTempCelciusSensor<double>{LM35_UUID} {};
+    virtual ~LM35() = default;
+
+    virtual void elaborateNewValue() override;
 };
 //----------------------------------------------------------------------------
 
-
-#endif  /*  __SENSOR_HPP__  */
+#endif /*  __SENSOR_HPP__  */
--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -1,18 +1,17 @@
 #include "Sensor.hpp"
 
 //-----------------------------------------------------------------------------
-void    ADT7310::elaborateNewValue(){
-    uint16_t    Temp;
+void ADT7310::elaborateNewValue() {
+    uint16_t Temp;
     // Crée un générateur de nombres pseudo-aléatoires avec un "seed" unique pour chaque exécution
-    std::random_device rd;  
-    std::mt19937 gen(rd()); 
+    std::random_device rd;
+    std::mt19937 gen(rd());
 
     // Définir une distribution uniforme entre ...
-    if (this->getMode() == ADT7310Mode::MODE13BITS){
+    if (this->getMode() == ADT7310Mode::MODE13BITS) {
         std::uniform_int_distribution<uint16_t> distrib(0, 0x1FFF);
         Temp = distrib(gen);
-    } 
-    else{
+    } else {
         std::uniform_int_distribution<uint16_t> distrib(0, 0xFFFF);
         Temp = distrib(gen);
     }
@@ -20,28 +19,31 @@ void    ADT7310::elaborateNewValue(){
     this->setOutputValue(Temp);
 }
 //-----------------------------------------------------------------------------
-double  ADT7310::getConvertedTemperature() const noexcept{
+double ADT7310::getConvertedTemperature() const noexcept {
     double Temp;
-    uint16_t    rawTemp = this->getOutputValue();
+    uint16_t rawTemp = this->getOutputValue();
 
-    if (this->getMode() == ADT7310Mode::MODE13BITS){
-        if (rawTemp < 0x1000) Temp = rawTemp / 16.0;        // Temperature is positive
-        else Temp = (rawTemp - 8192) / 16.0;                 // Temperature is negative
-    }
-    else{
-        if (rawTemp < 0x8000) Temp = rawTemp / 128.0;        // Temperature is positive
-        else Temp = (rawTemp - 65536) / 128.0;                 // Temperature is negative
+    if (this->getMode() == ADT7310Mode::MODE13BITS) {
+        if (rawTemp < 0x1000)
+            Temp = rawTemp / 16.0;   // Temperature is positive
+        else
+            Temp = (rawTemp - 8192) / 16.0;   // Temperature is negative
+    } else {
+        if (rawTemp < 0x8000)
+            Temp = rawTemp / 128.0;   // Temperature is positive
+        else
+            Temp = (rawTemp - 65536) / 128.0;   // Temperature is negative
     }
 
-    return Temp;    
+    return Temp;
 }
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
-void    TC74::elaborateNewValue(){
+void TC74::elaborateNewValue() {
 
     // Crée un générateur de nombres pseudo-aléatoires avec un "seed" unique pour chaque exécution
-    std::random_device rd;  
-    std::mt19937 gen(rd()); 
+    std::random_device rd;
+    std::mt19937 gen(rd());
 
     // Définir une distribution uniforme entre -65 et 127
     std::uniform_int_distribution<int> distrib(-65, 127);
@@ -53,11 +55,11 @@ void    TC74::elaborateNewValue(){
 }
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
-void    LM35::elaborateNewValue(){
+void LM35::elaborateNewValue() {
 
     // Crée un générateur de nombres pseudo-aléatoires avec un "seed" unique pour chaque exécution
-    std::random_device rd;  
-    std::mt19937 gen(rd()); 
+    std::random_device rd;
+    std::mt19937 gen(rd());
 
     // Définir une distribution uniforme entre -55 et 150
     std::uniform_real_distribution<double> distrib(-55.0, 150.0);
@@ -67,4 +69,3 @@ void    LM35::elaborateNewValue(){
     this->setOutputValue(Temp);
 }
 //-----------------------------------------------------------------------------
-
